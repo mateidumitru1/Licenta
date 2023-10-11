9a4617a8f294655e08f7f3ee8a90f163eb1af32b
mateidumitru1 <matei_ioan.dumitru@stud.acs.upb.ro>
2023-10-11 17:52:48 +0300

    Modify fill-fields-warning component

--- frontend/src/app/register/register.component.ts
+++ frontend/src/app/register/register.component.ts
@@ -13,16 +13,20 @@ export class RegisterComponent{
   username: string = '';
   password: string = '';
   email: string = '';
+  confirmPassword: string = '';
+
+  errorMessage: string = '';
   invalidRegister: boolean = false;
 
   constructor(private http: HttpClient) { }
 
   onRegister(): void {
     if(this.firstName === '' || this.lastName === '' || this.username === '' || this.password === '' || this.email === '') {
-      this.invalidRegister = true;
-      setTimeout(() => {
-        this.invalidRegister = false;
-      }, 1000);
+      this.timeout('Please fill out all fields');
+      return;
+    }
+    if(this.password !== this.confirmPassword) {
+      this.timeout('Passwords do not match!');
       return;
     }
     this.http.post('http://localhost:8080/api/register', {
@@ -34,5 +38,13 @@ export class RegisterComponent{
     }).subscribe((response: any) => {
       console.log(response);
     });
+  }
+
+  timeout(message: string): void {
+    this.invalidRegister = true;
+    this.errorMessage = message;
+    setTimeout(() => {
+      this.invalidRegister = false;
+    }, 1000);
   }
 }
