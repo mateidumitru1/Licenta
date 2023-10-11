c8e1edbda98293f6d366deb1dff747bc65181272
mateidumitru1 <matei_ioan.dumitru@stud.acs.upb.ro>
2023-10-11 16:24:46 +0300

    Change endpoints urls

--- backend/src/main/java/com/matei/backend/configuration/SecurityConfiguration.java
+++ backend/src/main/java/com/matei/backend/configuration/SecurityConfiguration.java
@@ -24,7 +24,7 @@ public class SecurityConfiguration {
         http
                 .csrf(AbstractHttpConfigurer::disable)
                 .authorizeHttpRequests(auth -> auth
-                        .requestMatchers("/api/auth/**")
+                        .requestMatchers("/api/**")
                         .permitAll()
                         .anyRequest()
                         .authenticated()
--- backend/src/main/java/com/matei/backend/controller/AuthenticationController.java
+++ backend/src/main/java/com/matei/backend/controller/AuthenticationController.java
@@ -5,15 +5,13 @@ import com.matei.backend.dto.request.RegisterRequestDto;
 import com.matei.backend.dto.response.AuthenticationResponseDto;
 import com.matei.backend.service.AuthenticationService;
 import lombok.RequiredArgsConstructor;
+import org.slf4j.Logger;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequiredArgsConstructor
-@RequestMapping("/api/auth")
+@RequestMapping("/api")
 public class AuthenticationController {
 
     private final AuthenticationService authenticationService;
